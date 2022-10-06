2facd1c833129384da273fa3d14f3bfdd7ee96e7
Ethan McIntyre <EthanMcIntyre@cmail.carleton.ca>
2022-10-06 16:40:50 -0400

    last bit to go

--- app/src/main/java/com/example/imd4008_a1/MainActivity.java
+++ app/src/main/java/com/example/imd4008_a1/MainActivity.java
@@ -2,8 +2,8 @@ package com.example.imd4008_a1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Debug;
 import android.util.Log;
 import android.view.HapticFeedbackConstants;
 import android.view.View;
@@ -15,7 +15,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    // displays
+    TextView calcDisplay;
+    TextView historyView;
+
+    // arraylists to store data
+    List<String> calcEntries;
+    List<String> history;
+
+    String answer;
+    boolean isDisplayingAnswer;
 
     // operator buttons
     Button addBtn;
@@ -39,56 +49,146 @@ public class MainActivity extends AppCompatActivity {
     Button clearBtn;
     Button deleteBtn;
     Button equalsBtn;
-
-    // displays
-    TextView calcDisplay;
-    TextView historyView;
-
-    List<String> calcEntries;
-    List<String> history;
-
+    Button signBtn;
+    Button deciBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        isDisplayingAnswer = false;
+
+        // setting up calculator display
         calcDisplay = findViewById(R.id.calcDisplay);
+        calcEntries = new ArrayList<>();
 
-        // setting up where calculator entries will be stored
-        calcEntries = new ArrayList<String>();
+        // setting up the history view
+        historyView = findViewById(R.id.historyView);
+        history = new ArrayList<>();
 
-
-        // getting views
+        // getting buttons and setting on click listener
         addBtn = findViewById(R.id.addBtn);
+        addBtn.setOnClickListener(this);
         subBtn = findViewById(R.id.subBtn);
+        subBtn.setOnClickListener(this);
         multiBtn = findViewById(R.id.multiBtn);
+        multiBtn.setOnClickListener(this);
         divBtn = findViewById(R.id.divBtn);
+        divBtn.setOnClickListener(this);
 
         nineBtn = findViewById(R.id.nineBtn);
+        nineBtn.setOnClickListener(this);
         eightBtn = findViewById(R.id.eightBtn);
+        eightBtn.setOnClickListener(this);
         sevenBtn = findViewById(R.id.sevenBtn);
+        sevenBtn.setOnClickListener(this);
         sixBtn = findViewById(R.id.sixBtn);
+        sixBtn.setOnClickListener(this);
         fiveBtn = findViewById(R.id.fiveBtn);
+        fiveBtn.setOnClickListener(this);
         fourBtn = findViewById(R.id.fourBtn);
+        fourBtn.setOnClickListener(this);
         threeBtn = findViewById(R.id.threeBtn);
+        threeBtn.setOnClickListener(this);
         twoBtn = findViewById(R.id.twoBtn);
+        twoBtn.setOnClickListener(this);
         oneBtn = findViewById(R.id.oneBtn);
+        oneBtn.setOnClickListener(this);
         zeroBtn = findViewById(R.id.zeroBtn);
+        zeroBtn.setOnClickListener(this);
 
         clearBtn = findViewById(R.id.clearBtn);
+        clearBtn.setOnClickListener(this);
         deleteBtn = findViewById(R.id.delBtn);
+        deleteBtn.setOnClickListener(this);
         equalsBtn = findViewById(R.id.equalsBtn);
+        equalsBtn.setOnClickListener(this);
+        signBtn = findViewById(R.id.signBtn);
+        signBtn.setOnClickListener(this);
+        deciBtn = findViewById(R.id.deciBtn);
+        deciBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putStringArrayList("history", (ArrayList<String>) history);
+        savedInstanceState.putStringArrayList("calcEntries", (ArrayList<String>) calcEntries);
+        savedInstanceState.putBoolean("isDisplayingAnswer", isDisplayingAnswer);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        history = savedInstanceState.getStringArrayList("history");
+        calcEntries = savedInstanceState.getStringArrayList("calcEntries");
+        isDisplayingAnswer = savedInstanceState.getBoolean("isDisplayingAnswer");
+        updateHistoryDisplay();
+        if(isDisplayingAnswer) calcDisplay.setText(answer);
+        else updateCalcDisplay();
+    }
+
+    @Override
+    public void onClick(View v) {
+        // using haptic feedback for every button
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            v.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_PRESS);
+        }
+        isDisplayingAnswer = false;
+        int id = v.getId();
+        if (id == R.id.nineBtn) {
+            addEntry('9');
+        } else if (id == R.id.eightBtn) {
+            addEntry('8');
+        } else if (id == R.id.sevenBtn) {
+            addEntry('7');
+        } else if (id == R.id.sixBtn) {
+            addEntry('6');
+        } else if (id == R.id.fiveBtn) {
+            addEntry('5');
+        } else if (id == R.id.fourBtn) {
+            addEntry('4');
+        } else if (id == R.id.threeBtn) {
+            addEntry('3');
+        } else if (id == R.id.twoBtn) {
+            addEntry('2');
+        } else if (id == R.id.oneBtn) {
+            addEntry('1');
+        } else if (id == R.id.zeroBtn) {
+            addEntry('0');
+        } else if (id == R.id.addBtn) {
+            addEntry('+');
+        } else if (id == R.id.subBtn) {
+            addEntry('-');
+        } else if (id == R.id.multiBtn) {
+            addEntry('*');
+        } else if (id == R.id.divBtn) {
+            addEntry('/');
+        } else if (id == R.id.deciBtn) {
+            addDecimal();
+        } else if (id == R.id.signBtn) {
+            changeSign();
+        } else if (id == R.id.delBtn) {
+            delEntry();
+        } else if (id == R.id.clearBtn) {
+            clearEntry();
+        } else if (id == R.id.equalsBtn) {
+            calculate();
+            isDisplayingAnswer = true;
+        } else {
+            Log.e("BUTTON", "Unidentified button pressed");
+        }
+        if(isDisplayingAnswer) updateCalcDisplay();
     }
 
     public void addEntry(char entry) {
-        Log.d("ENTRY", String.valueOf(entry));
         if(Character.isDigit(entry) && Character.getNumericValue(entry) < 10) {
             if (calcEntries.size() == 0) {
                 calcEntries.add(Character.toString(entry));
             } else {
+                // getting the last entry in the calcEntries list
                 String lastEntry = calcEntries.get(calcEntries.size() - 1);
-                //Log.d("ENTRY", lastEntry);
                 if (!Character.isDigit(lastEntry.charAt(0)) && lastEntry.length() == 1) {
                     // char is an operator
                     calcEntries.add(Character.toString(entry));
@@ -101,7 +201,7 @@ public class MainActivity extends AppCompatActivity {
                     if(lastEntry.charAt(0) == '0' && lastEntry.length() == 1 && entry == '0' ) return;
 
                     calcEntries.set(calcEntries.size() - 1,
-                            calcEntries.get(calcEntries.size() - 1) + Character.toString(entry));
+                            calcEntries.get(calcEntries.size() - 1) +entry);
                 }
             }
         }
@@ -111,7 +211,6 @@ public class MainActivity extends AppCompatActivity {
                 calcEntries.add(Character.toString(entry));
             }
         }
-
     }
 
     public void delEntry() {
@@ -136,7 +235,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clearEntry() {
-        calcEntries = new ArrayList<String>();
+        calcEntries = new ArrayList<>();
     }
 
     public void changeSign() {
@@ -153,14 +252,20 @@ public class MainActivity extends AppCompatActivity {
                 } else if (lastEntry.length() > 1) {
                     // there is a sign, remove the negative
                     calcEntries.set(calcEntries.size() - 1,
-                            lastEntry.substring(1, lastEntry.length()));
+                            lastEntry.substring(1));
                 }
             }
         }
     }
 
     public void calculate() {
-        float result;
+        if(calcEntries.size() <= 0) return;
+
+        String fullEquation = "";
+        for(String entry : calcEntries) {
+            fullEquation = fullEquation.concat(entry);
+        }
+
         // trimming undesired operators at the end
         if(calcEntries.get(calcEntries.size() - 1).length() == 1
                 && !Character.isDigit(calcEntries.get(calcEntries.size() - 1).charAt(0))) {
@@ -210,7 +315,13 @@ public class MainActivity extends AppCompatActivity {
 
             if(!subResult.equals("")) calcEntries.add(insertIndex - 1, subResult);
         }
-        Log.d("CALC", String.valueOf(calcEntries));
+
+        answer = String.valueOf(calcEntries.get(0));
+        Log.d("ANSWER", answer);
+        calcDisplay.setText(answer);
+        history.add(0, fullEquation + " = " + calcEntries.get(0));
+
+        updateHistoryDisplay();
         clearEntry();
     }
 
@@ -219,7 +330,6 @@ public class MainActivity extends AppCompatActivity {
         if(calcEntries.size() == 0) {
             calcEntries.add("0.");
         } else if (!calcEntries.get(calcEntries.size() - 1).contains(".")) {
-            Log.d("DECIMAL", "no decimal yet");
             String lastEntry = calcEntries.get(calcEntries.size() - 1);
             if (Character.isDigit(lastEntry.charAt(0)) || lastEntry.length() != 1) {
                 calcEntries.set(calcEntries.size() - 1, lastEntry + ".");
@@ -230,78 +340,16 @@ public class MainActivity extends AppCompatActivity {
     public void updateCalcDisplay() {
         String out = "";
         for(String entry : calcEntries) {
-            out = out + " " + entry;
+            out = out.concat(" " + entry);
         }
         calcDisplay.setText(out);
     }
 
-    public void buttonClicked(View v) {
-        // using haptic feedback for every button
-        v.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_PRESS);
-        boolean equalsPressed = false;
-        switch (v.getId()) {
-            case R.id.nineBtn:
-                addEntry('9');
-                break;
-            case R.id.eightBtn:
-                addEntry('8');
-                break;
-            case R.id.sevenBtn:
-                addEntry('7');
-                break;
-            case R.id.sixBtn:
-                addEntry('6');
-                break;
-            case R.id.fiveBtn:
-                addEntry('5');
-                break;
-            case R.id.fourBtn:
-                addEntry('4');
-                break;
-            case R.id.threeBtn:
-                addEntry('3');
-                break;
-            case R.id.twoBtn:
-                addEntry('2');
-                break;
-            case R.id.oneBtn:
-                addEntry('1');
-                break;
-            case R.id.zeroBtn:
-                addEntry('0');
-                break;
-            case R.id.addBtn:
-                addEntry('+');
-                break;
-            case R.id.subBtn:
-                addEntry('-');
-                break;
-            case R.id.multiBtn:
-                addEntry('*');
-                break;
-            case R.id.divBtn:
-                addEntry('/');
-                break;
-            case R.id.deciBtn:
-                addDecimal();
-                break;
-            case R.id.signBtn:
-                changeSign();
-                break;
-            case R.id.delBtn:
-                delEntry();
-                break;
-            case R.id.clearBtn:
-                clearEntry();
-                break;
-            case R.id.equalsBtn:
-                calculate();
-                equalsPressed = true;
-                break;
-            default:
-                Log.e("BUTTON", "Unidentified button pressed");
+    public void updateHistoryDisplay() {
+        String out = "";
+        for(String item : history) {
+            out = out.concat(item + "\n");
         }
-        Log.d("STATE", String.valueOf(calcEntries));
-        if(!equalsPressed) updateCalcDisplay();
+        historyView.setText(out);
     }
 }
